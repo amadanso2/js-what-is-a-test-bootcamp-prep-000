939032c20b61324c8439bec818d88a3ca9b7fded
Ama Danso <ama5danso@yahoo.com>
2018-10-08 07:39:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var index = require("../index.js");
  describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
-    })
-  })
+      expect(index.name).toEqual('Susan');
+    });
+  });
   describe("Height", () => {
     it("is less than 40", () => {
-      expect(index.height).toBeLessThan(40)
-    })
-  })
+      expect(index.height).toBeLessThan(40);
+    });
+  });
